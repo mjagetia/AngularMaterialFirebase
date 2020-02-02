963e2e5a9481146de300bcef42f5143184312314
Paarth Jagetia <paarth@jagetia.family>
2020-02-01 21:50:49 -0800

    Step2

--- src/app/components/blocks/first-block/first-block.component.ts
+++ src/app/components/blocks/first-block/first-block.component.ts
@@ -10,13 +10,14 @@ import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 // import Timestamp = firebase.firestore.Timestamp;
 import { Timestamp } from '@firebase/firestore-types';
+import FieldValue = firebase.firestore.FieldValue;
 
 export interface Item { Name: string; }
 
 export interface User {
   punchType?: string;
   currentState: string;
-  currentStateTime: Timestamp;
+  currentStateTime: FieldValue;
   displayName: string;
   localTime: Timestamp;
   serverTime: Timestamp;
@@ -61,8 +62,8 @@ export class FirstBlockComponent implements OnInit {
 
   items: Observable<Item[]>;
   users: Observable<User[]>;
-  private uid: string = '??';
-  private iname: string = 'Unavailable';
+  private uid = '??';
+  private iname = 'Unavailable';
   private user: Observable<User>;
   private punchCard: Observable<PunchCard>;
   private userId: string;
@@ -75,18 +76,19 @@ export class FirstBlockComponent implements OnInit {
   private myPunchCard: PunchCard;
   curTime: any;
   private items$: any;
+  private puchData: Observable<PunchCardHistory[]>;
 
- /* transform(timestamp: Timestamp, format?: string): string {
-    return formatDate(timestamp.toDate(), format || 'medium', this.locale);
-  }*/
+  /* transform(timestamp: Timestamp, format?: string): string {
+     return formatDate(timestamp.toDate(), format || 'medium', this.locale);
+   }*/
 
   updateTime() {
 
-      this.curTime = new Date();
-      // console.log(this.curTime);
-      setTimeout( () => {
-        this.updateTime();
-      }, 1000);
+    this.curTime = new Date();
+    // console.log(this.curTime);
+    setTimeout(() => {
+      this.updateTime();
+    }, 1000);
 
   }
 
@@ -94,42 +96,42 @@ export class FirstBlockComponent implements OnInit {
               private afs: AngularFirestore, private afAuth: AngularFireAuth) {
   }
 
-/*
-  clockIn(){
-    this.userDoc = afs.doc<Item>('user/david');
-    this.tasks = this.userDoc.collection<User>('Users/' + this.userId).valueChanges();
+  /*
+    clockIn(){
+      this.userDoc = afs.doc<Item>('user/david');
+      this.tasks = this.userDoc.collection<User>('Users/' + this.userId).valueChanges();
+    }
+  */
+
+  public changeValue(event, punchType) {
+    console.log(event);
+    console.log(punchType);
+    this.myUser.currentState = '5';
+    this.punchType = punchType;
+    this.myUser.punchType = this.punchType;
+    this.myUser.localTime = firebase.firestore.Timestamp.fromDate(new Date('December 10, 1815'));
+    this.myUser.serverTime = firebase.firestore.FieldValue.serverTimestamp();
+    this.myUser.currentStateTime = firebase.firestore.FieldValue.serverTimestamp();
+
+    console.log(this.myUser);
+    this.userDoc.set(this.myUser, {merge: true});
+    // let punch = new Punch
+    // const newPunch = this.afs.collection('PunchCardHistory').doc();
+    // newPunch.set({uid: this.userId, time: firebase.firestore.FieldValue.serverTimestamp()});
+    const punchCardHistory = {
+      uid: this.userId,
+      time: firebase.firestore.FieldValue.serverTimestamp(),
+      punchType: this.punchType,
+      displayName: this.userName
+    };
+    this.afs.collection('PunchCardHistory').add(punchCardHistory);
+
+    // this.punchCardHistoryDoc = this.afs.doc<PunchCardHistory>('PunchCardHistory/' + this.userId + '');
+    // this.punchCard = this.punchCardDoc.valueChanges();
+    // this.punchCardDoc = this.afs.doc<PunchCard>('PunchCard/');
+    // this.punchCard = this.punchCardDoc.valueChanges();
+
   }
-*/
-
-public changeValue(event, punchType) {
-  console.log(event);
-  console.log(punchType);
-  this.myUser.currentState = '5';
-  this.punchType = punchType;
-  this.myUser.punchType = this.punchType;
-  this.myUser.localTime = firebase.firestore.Timestamp.fromDate(new Date('December 10, 1815'));
-  this.myUser.serverTime = firebase.firestore.FieldValue.serverTimestamp();
-  this.myUser.currentStateTime = firebase.firestore.FieldValue.serverTimestamp();
-
-  console.log(this.myUser);
-  this.userDoc.set(this.myUser, { merge: true });
-  //let punch = new Punch
-  // const newPunch = this.afs.collection('PunchCardHistory').doc();
-  // newPunch.set({uid: this.userId, time: firebase.firestore.FieldValue.serverTimestamp()});
-  const punchCardHistory = {
-    uid: this.userId,
-    time: firebase.firestore.FieldValue.serverTimestamp(),
-    punchType: this.punchType,
-    displayName: this.userName
-  };
-  this.afs.collection('PunchCardHistory').add(punchCardHistory);
-
-  //this.punchCardHistoryDoc = this.afs.doc<PunchCardHistory>('PunchCardHistory/' + this.userId + '');
-  // this.punchCard = this.punchCardDoc.valueChanges();
-  // this.punchCardDoc = this.afs.doc<PunchCard>('PunchCard/');
-  // this.punchCard = this.punchCardDoc.valueChanges();
-
-}
 
 
   ngOnInit(): void {
@@ -139,75 +141,82 @@ public changeValue(event, punchType) {
       if (user) {
         this.userId = user.uid;
         this.userName = user.displayName;
-      }
-      // this.usersCollection = this.afs.collection<User>('Users');
-      // console.log(this.usersCollection);
-      this.userDoc = this.afs.doc<User>('Users/' + this.userId);
-      this.user = this.userDoc.valueChanges();
 
-      this.user.subscribe(user => {
-        console.log(user);
-        this.myUser = user;
-        if (user && user.serverTime) {
-          this.myUser.serverTimeJS = user.serverTime.toDate();
-        }
-        //this.myUser.theString = JSON.stringify(user);
-        }
-      );
-      // this.groupDoc = this.afs.doc<Group>('Groups/KbhWIJPjUcfjRtrxCAyP');
-      // this.group = this.groupDoc.valueChanges();
+        // this.usersCollection = this.afs.collection<User>('Users');
+        // console.log(this.usersCollection);
+        this.userDoc = this.afs.doc<User>('Users/' + this.userId);
+        this.user = this.userDoc.valueChanges();
 
-
-
-      this.punchCardDoc = this.afs.doc<PunchCard>('PunchCard/' + this.userId);
-      this.punchCard = this.punchCardDoc.valueChanges();
-
-      this.punchCard.subscribe(punchCard => {
-          if (punchCard === undefined) {
-
-          // TODO open popup to create new account for first time punching
-
-          } else {
-            console.log(punchCard);
-            this.myPunchCard = punchCard;
-            this.myPunchCard.serverTime = firebase.firestore.FieldValue.serverTimestamp();
-            this.myPunchCard.time = new Date();
-
-
-/*
-            const this.items$ = this.afs.collection('PunchCardHistory', ref => {
-              let query : firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
-               query = query.where("uid", "==", this.userId);
-              return query;
-            })
-              .valueChanges();
-              */
-/*
-            // Create a reference to the cities collection
-            var punchCardHistoryRef = this.afs.collection("PunchCardHistory");
-
-// Create a query against the collection.
-            var query = punchCardHistoryRef.where("uid", "==", this.userId)
-
-              .get()
-              .then(function(querySnapshot) {
-                querySnapshot.forEach(function(doc) {
-                  // doc.data() is never undefined for query doc snapshots
-                  console.log(doc.id, " => ", doc.data());
-                });
-              })
-              .catch(function(error) {
-                console.log("Error getting documents: ", error);
-              });*/
-
+        this.user.subscribe(user => {
+          console.log(user);
+          this.myUser = user;
+          if (user && user.serverTime && user.serverTime.seconds) {
+            this.myUser.serverTimeJS = new Date(user.serverTime.seconds);
           }
-        }
-      );
+          // this.myUser.theString = JSON.stringify(user);
+          // this.groupDoc = this.afs.doc<Group>('Groups/KbhWIJPjUcfjRtrxCAyP');
+          // this.group = this.groupDoc.valueChanges();
 
 
+          const td = new Date();
+          td.setDate(1);
+          td.setHours(0, 0, 0, 0);
+          let myp;
+          // ref.orderByChild("lastUpdatedTimestamp").startAt("1490187991");
+          myp = this.afs.collection('PunchCardHistory',
+              ref => ref.where('uid', '==', this.userId)
+                // .startAt('2017-11-08T01:00:00+01:00')
+               .where('time', '>', firebase.firestore.Timestamp.fromMillis(
+                 Date.now() - (7 * 24 * 60 * 1000)))
+                .limit(3)
+               .orderBy('time', 'desc')
+                );
 
+          this.puchData = myp.valueChanges();
+          console.log(myp);
+
+          this.punchCardDoc = this.afs.doc<PunchCard>('PunchCard/' + this.userId);
+          this.punchCard = this.punchCardDoc.valueChanges();
+
+          this.punchCard.subscribe(punchCard => {
+            if (punchCard === undefined) {
+              // TODO open popup to create new account for first time punching
+            } else {
+              console.log(punchCard);
+              this.myPunchCard = punchCard;
+              this.myPunchCard.serverTime = firebase.firestore.FieldValue.serverTimestamp();
+              this.myPunchCard.time = new Date();
+
+
+              /*
+                          const this.items$ = this.afs.collection('PunchCardHistory', ref => {
+                            let query : firebase.firestore.CollectionReference | firebase.firestore.Query = ref;
+                             query = query.where("uid", "==", this.userId);
+                            return query;
+                          })
+                            .valueChanges();
+                            */
+              /*
+                          // Create a reference to the cities collection
+                          var punchCardHistoryRef = this.afs.collection("PunchCardHistory");
+
+              // Create a query against the collection.
+                          var query = punchCardHistoryRef.where("uid", "==", this.userId)
+
+                            .get()
+                            .then(function(querySnapshot) {
+                              querySnapshot.forEach(function(doc) {
+                                // doc.data() is never undefined for query doc snapshots
+                                console.log(doc.id, " => ", doc.data());
+                              });
+                            })
+                            .catch(function(error) {
+                              console.log("Error getting documents: ", error);
+                            });*/
+            }
+          });
+        });
+      }
     });
-
   }
-
 }
